e01729a991fcebc126bed1b95b57819e3b8adb25
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-10-31 22:14:38 +0300

    Utils (tokenize function and generate vocab)

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import re
+
 
 def map_classes(score):
     if 0 <= score <= 0.2:
@@ -12,12 +14,35 @@ def map_classes(score):
     elif 0.8 < score <= 1.0:
         return 4  # Very positive
     else:
-        return None  # If score is out of range
+        return None
+
+
+def tokenize(text):
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove punctuation and special characters, and exclude numbers
+    text = re.sub(r"[^a-zA-Z\s]", "", text)  # Keep only letters and spaces
+
+    # Handle multiple spaces
+    text = re.sub(
+        r"\s+", " ", text
+    ).strip()  # Replace multiple spaces with a single space
+
+    text = re.sub(r"[^\w\s]", "", text)
+
+    # Split into tokens
+    tokens = text.split()
+
+    # Remove any empty tokens (in case of leading/trailing spaces)
+    tokens = [token for token in tokens if token]
+    
+    return tokens
+
 
 def generate_vocab(train_df):
-    tokens = []
+    tokens = set()
     for _, row in train_df.iterrows():
-        row_tokens = row['tokens'].split('|')
-        tokens.extend(row_tokens)
-    return np.unique(tokens)    
-
+        row_tokens = tokenize(row["sentence"])
+        tokens.update(row_tokens)
+    return np.array(sorted(tokens))
